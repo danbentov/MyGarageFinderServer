38fe21cd4b652cddfe8381edf9f3c77459d280e6
DAN BEN TOV <danbentov3@gmail.com>
2024-11-16 10:23:52 +0200

    nothing

--- MyGarageFinderServer/Controllers/MyGarageFinderAPIController.cs
+++ MyGarageFinderServer/Controllers/MyGarageFinderAPIController.cs
@@ -217,5 +217,77 @@ public class MyGarageFinderAPIController : ControllerBase
 
 
 
+    //[HttpPost("getCarRepairs")]
+    //public IActionResult GetAllCarRepairs([FromBody] TheGarageManagerServer.DTO.LicensePlateDTO licensePlateDto)
+    //{
+    //    try
+    //    {
+    //        string l = licensePlateDto.LicensePlate;
+    //        ObservableCollection<CarRepair> vehicleRepairs = context.GetRepairs(l);
+    //        ObservableCollection<CarRepairDTO> vehicleRepairsDto = new ObservableCollection<CarRepairDTO>();
+    //        foreach (CarRepair v in vehicleRepairs)
+    //        {
+    //            CarRepairDTO vDto = new CarRepairDTO(v);
+    //            vehicleRepairsDto.Add(vDto);
+    //        }
+    //        return Ok(vehicleRepairsDto);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        return BadRequest(ex.Message);
+    //    }
+    //}
+
+    //public ObservableCollection<CarRepair> GetRepairs(string licensePlate)
+    //{
+    //    ObservableCollection<CarRepair> result = new ObservableCollection<CarRepair>();
+    //    foreach (CarRepair v in this.CarRepairs)
+    //    {
+    //        if (v.LicensePlate == licensePlate)
+    //        {
+    //            result.Add(v);
+    //        }
+    //    }
+    //    return result;
+    //}
+
+    //public class CarRepairDTO
+    //{
+    //    public int RepairID { get; set; }
+    //    public string LicensePlate { get; set; }
+    //    public int? GarageID { get; set; }
+    //    public DateTime? RepairDate { get; set; }
+    //    public string DescriptionCar { get; set; }
+    //    public int? Cost { get; set; }
+
+    //    public CarRepairDTO() { }
+
+    //    public CarRepairDTO(Models.CarRepair modelRepair)
+    //    {
+    //        this.RepairID = modelRepair.RepairId;
+    //        this.LicensePlate = modelRepair.LicensePlate;
+    //        this.GarageID = modelRepair.GarageId;
+    //        this.RepairDate = modelRepair.RepairDate;
+    //        this.DescriptionCar = modelRepair.DescriptionCar;
+    //        this.Cost = modelRepair.Cost;
+    //    }
+
+    //    public Models.CarRepair GetModels()
+    //    {
+    //        Models.CarRepair modelsRepair = new Models.CarRepair()
+    //        {
+    //            RepairId = this.RepairID,
+    //            LicensePlate = this.LicensePlate,
+    //            GarageId = this.GarageID,
+    //            RepairDate = this.RepairDate,
+    //            DescriptionCar = this.DescriptionCar,
+    //            Cost = this.Cost
+    //        };
+    //        return modelsRepair;
+    //    }
+    //}
+
+
+
 
 }
